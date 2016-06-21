5f6fad7167052cc35bbae23f013fbe7665b80187
David Kumru <davidkumru@gmail.com>
2016-06-21 15:22:23 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.delete_all
+
+Post.create( title: "My first post.", content: "Pizza is yummy!")
+Post.create( title: "My second post.", content: "Pizza is yummy!")
+Post.create( title: "My third post.", content: "Pizza is yummy!")
