19118b782e84122e633f45471498289b91e0a7df
David Kumru <davidkumru@gmail.com>
2016-06-21 17:11:28 +0200

    toggle favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-   post = Post.find( params[:post_id] )
-   favorite = Favorite.new( post: post, user: current_user )
+     post = Post.find( params[:post_id] )
 
-   favorite.save
+     if favorite = Favorite.find_by( post: post, user: current_user )
+        favorite.destroy
+     else
+        favorite = Favorite.new( post: post, user: current_user )
+        favorite.save
+     end
 
-   redirect_to posts_path
+     redirect_to posts_path
   end
 end
