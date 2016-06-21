c2886ce495c7cbddc67ddf8456dbece3379c3f84
David Kumru <davidkumru@gmail.com>
2016-06-21 15:56:01 +0200

    add user to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,6 @@ anton = User.create( email: 'anton@mail.com', password: 'abcd' )
 
 Post.delete_all
 
-Post.create( title: "My first post.", content: "Pizza is yummy!")
-Post.create( title: "My second post.", content: "Pasta is yummy!")
-Post.create( title: "My third post.", content: "Rice is yummy!")
+Post.create( title: "My first post.", content: "Pizza is yummy!", user: frank)
+Post.create( title: "My second post.", content: "Pasta is yummy!", user: alice)
+Post.create( title: "My third post.", content: "Rice is yummy!", user: anton)
