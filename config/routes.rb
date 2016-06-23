a4fc69db2afde3a698d3ec8ebc0d382101308d77
David Kumru <davidkumru@gmail.com>
2016-06-23 16:40:20 +0200

    added tagging functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :favorites
     resources :comments
   end
+  resources :tags
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
